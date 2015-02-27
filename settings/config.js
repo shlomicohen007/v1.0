a5124cab0857b6dfec6997549962561199206418
Yehuda Tuvia <ytuvia@gmail.com>
2015-02-27 18:27:41 +0200

    go to port 80

--- settings/config.js
+++ settings/config.js
@@ -2,11 +2,11 @@
     autoPack:true,
     listen:{
 	    host: "localhost",
-	    port: 3002
+	    port: 80
     },
     serve:{
         host: "localhost",
-	    port: 3002
+	    port: 80
     },
     db:{name:"BusNet"}
 }
