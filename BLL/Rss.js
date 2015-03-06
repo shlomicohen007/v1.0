8b3edf92c034869fae6bbd0a07b27801c8800149
Yehuda Tuvia <ytuvia@gmail.com>
2015-03-06 19:41:17 +0200

    mako news in hebrew

--- BLL/Rss.js
+++ BLL/Rss.js
@@ -1,14 +1,18 @@
 ﻿var fs = require('fs'),
  XML = require('xml-simple');
 var request = require("request");
-var windows1255 = require('windows-1255');
+//var windows1255 = require('windows-1255');
+var encoding = require('encoding');
 
 var rss = {
     getRss: function (cb) {
 
-
-        request('http://www.ynet.co.il/Integration/StoryRss1854.xml', function (error, response, body) {
-            //var rss = windows1255.decode(body);
+        request({
+            url: 'http://rcs.mako.co.il/rss/31750a2610f26110VgnVCM1000005201000aRCRD.xml'
+        }, function (error, response, body) {
+            
+            //var rss = encoding.convert(body,'utf-8','windows-1252');
+            //console.log(rss);
             XML.parse(body, function (e, parsed) {
                 if (parsed)
                     cb(e, { res: parsed.channel.item });
