46eda02b4e9e132f2fdba35590d581588e370cda
Yehuda Tuvia <ytuvia@gmail.com>
2015-03-09 21:27:02 +0200

    trying load balancer

--- settings/config.js
+++ settings/config.js
@@ -1,14 +1,18 @@
 ﻿module.exports = {
     autoPack:true,
     listen:{
-	    host: "ec2-52-11-65-151.us-west-2.compute.amazonaws.com",
+	    //host: "ec2-52-11-65-151.us-west-2.compute.amazonaws.com",
         //host: "localhost",
-	    port: 3002
+        //port: 3002,
+        host: "awseb-e-g-AWSEBLoa-1323WP9X79QUC-1828159266.us-west-2.elb.amazonaws.com",
+	    port: 80
     },
     serve:{
-        host: "ec2-52-11-65-151.us-west-2.compute.amazonaws.com",
+        host: host: "ec2-52-11-65-151.us-west-2.compute.amazonaws.com",
         //host: "localhost",
-	    port: 3002
+        //port: 3002,
+        host: "awseb-e-g-AWSEBLoa-1323WP9X79QUC-1828159266.us-west-2.elb.amazonaws.com",
+        port: 80
     },
     db:{
         name:"BusNet",
